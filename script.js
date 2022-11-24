1e2838acaf66b5b5849ca614a0ff1eee2e6b310a
Duke <aboukhodr21@gmail.com>
2022-11-24 11:09:08 -0500

    code refactoring

--- script.js
+++ script.js
@@ -8,23 +8,14 @@ function Book(author, pageNumber, title) {
 }
 
 Book.prototype.read = false
-Book.prototype.toggleRead = () => this.read = !this.read;
-
 
 const titleInput = document.getElementById("title-input");
 const pageNumberInput = document.getElementById("pageNumber-input");
 const authorInput = document.getElementById("author-input");
 
 function addBookToLibrary(book) {
-  // do stuff here
   myLibrary.push(book);
 }
-
-const wind = new Book("duke", 20, "wind");
-const rain = new Book("steve", 40, "rain");
-addBookToLibrary(wind)
-addBookToLibrary(rain)
-arrayLoop()
 
 function arrayLoop() {
   const table = document.createElement("table");
@@ -44,8 +35,8 @@ function arrayLoop() {
 
 
     readButton.addEventListener("click", () => {
-      myLibrary[i].toggleRead()
-      console.log(myLibrary[i])
+      myLibrary[i].read = !myLibrary[i].read 
+       console.log(myLibrary[i])
       if (myLibrary[i].read === false) { 
         readButton.innerHTML = "Not Read"
       } else if (myLibrary[i].read === true) {
@@ -53,36 +44,24 @@ function arrayLoop() {
       } else {return}
     })
   
-    // readButton.setAttribute('style', )
-
     const button = document.createElement("button");
     button.innerHTML = "Delete";
     button.addEventListener("click", (evt) => {
       myLibrary.splice(i, 1)
       arrayLoop()
     });
-    const cell1 = document.createElement("td");
-    const cell2 = document.createElement("td");
-    const cell3 = document.createElement("td");
-    cell1.textContent = myLibrary[i].title
-    cell2.textContent = myLibrary[i].pageNumber
-    cell3.textContent = myLibrary[i].author
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    row.appendChild(button);
-    row.appendChild(readButton);
-    table.appendChild(row);
+  [myLibrary[i].title, myLibrary[i].pageNumber, myLibrary[i].author].forEach((prop) => { 
+    const cell = document.createElement('td')
+    cell.textContent = prop 
+    row.appendChild(cell)
+  })
+  row.appendChild(button);
+  row.appendChild(readButton);
+  table.appendChild(row);
   }
   document.getElementById("box").innerHTML = "";
   document.getElementById("box").appendChild(table)
-
- 
-  
-
 }
-
-
 
 window.addEventListener("load", () => {
   document.querySelector("form").addEventListener("submit", (evt) => {
@@ -100,5 +79,3 @@ window.addEventListener("load", () => {
    
   });
 });
-
-
